9a2dd2e4cb5367c0472780dfbc2b2658a87b49f9
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2023-03-29 15:37:43 +0000

    Connected a database to list jobs

--- app.py
+++ app.py
@@ -1,42 +1,21 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__ ,static_url_path='/static')
 
-JOBS = [
-  {
-    'id' : 1,
-    'title': 'Data Scienticst',
-    'location': 'Abuja',
-    'salary': '120,000.000'
-  },
-   {
-    'id' : 2,
-    'title': 'Data Analyst',
-    'location': 'Abuja',
-    'salary': '80,000.000'
-  },
- {
-    'id' : 3,
-    'title': 'Network Engineer',
-    'location': 'Remote',
-    'salary': '75,000.000'
-  },
- {
-    'id' : 4,
-    'title': 'Product Designer',
-    'location': 'Abuja',
-    'salary': '70,000.000'
-  }
-]
+
+
 
 @app.route("/")
 def hello():
+    jobs = load_jobs_from_db()
     return render_template('Home.html', 
-                           jobs=JOBS,
+                           jobs=jobs,
                           company_name='nRyde')
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
