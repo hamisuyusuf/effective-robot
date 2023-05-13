351cf57df26fe20c000d2c3b74789e9aecf20538
hamisuyusuf <111646226+hamisuyusuf@users.noreply.github.com>
2023-05-13 10:52:09 +0000

    Dynamic data added
    Dynamic data added through db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db ,load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 
 app = Flask(__name__ ,static_url_path='/static')
@@ -7,8 +7,8 @@ app = Flask(__name__ ,static_url_path='/static')
 
 @app.route("/")
 def hello():
-    jobs = load_jobs_from_db()
-    return render_template('Home.html', 
+  jobs = load_jobs_from_db()
+  return render_template('Home.html', 
                            jobs=jobs)
 
 @app.route("/api/jobs")
@@ -16,19 +16,31 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
-@app.route("/job/<int:id>")
+@app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
   if not job:
     return "Not Found", 404
     
   return render_template('jobpage.html',
-                        job=job)
+                                job=job)
 
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
   data = request.form
-  return jsonify(data)
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('app_sub.html',
+                        application=data,
+                        job=job)
+
+
+
+
+# @app.route("/admin")
+# def admin():
+  
+#     return render_template('admin.html')
   
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,17 +1,7 @@
 from sqlalchemy import create_engine, text
-from dataclasses import dataclass, asdict 
-
-@dataclass
-class Job:
-    id: int
-    title: str
-    salary: int
-    requirements: str
-    responsibilities: str
-    location: str
-    currency: str  
-
 import os
+
+
 db_connection_string =os.environ['DB_CONNECTION_STRING']
 engine = create_engine(
   db_connection_string,
@@ -24,30 +14,46 @@ engine = create_engine(
 
 def load_jobs_from_db():
     with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs"))
-        column_names = result.keys()
-        rows = result.fetchall()
-        jobs = [dict(zip(column_names, row)) 
-      for row in rows]
-        return jobs
+      
+      result = conn.execute(text("select * from jobs"))
+      #   column_names = result.keys()
+      #   rows = result.fetchall()
+      #   jobs = [dict(zip(column_names, row)) 
+      # for row in rows]
+      #   return jobs
+      jobs =[]
+      for row in result.all():
+        jobs.append(dict(row._mapping))
+      return jobs  
 
 
 
 
-def load_job_from_db(id):
+def  load_job_from_db(id):
     with engine.connect() as conn:
-        result = conn.execute(text("SELECT id, title, salary, requirements, responsibilities, location, currency FROM jobs WHERE id = :val"), {"val": id})
         
-    rows = result.fetchall()
+        result = conn.execute(
+          text(f"SELECT * FROM jobs WHERE id = :val"),
+          {"val": id}
+        )
+        
+    rows = result.mappings().all()
     if len(rows) == 0:
         return None
     else:
-        jobs = [Job(*row) for row in rows]
-        return [asdict(job) for job in jobs]
+      return dict(rows[0])
 
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, name, linkedIn_url, resume_link) VALUES (:job_id, :name, :email, :linkedIn_url, :resume_link)")
 
-
-
+    conn.execute(query, 
+             job_id=job_id,
+             name=data['name'],
+             email=data['email'],
+             linkedIn_url=data['linkedIn_url'],
+             resume_link=data['resume_link']
+            )
 
 
 
